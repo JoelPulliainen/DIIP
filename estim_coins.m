eb2c7a081ee32481b7b60373c9217f4c39ca19ba
JoelPulliainen <joelpulliainen@hotmail.com>
2023-01-13 02:07:07 +0200

    new stuff

--- estim_coins.m
+++ estim_coins.m
@@ -3,23 +3,6 @@ clear all
 close all
 
 
-bias1 = imread("_DSC1722.JPG");
-bias2 = imread("_DSC1723.JPG");
-bias3 = imread("_DSC1724.JPG");
-bias4 = imread("_DSC1725.JPG");
-bias5 = imread("_DSC1726.JPG");
-dark1 = imread("_DSC1762.JPG");
-dark2 = imread("_DSC1763.JPG");
-dark3 = imread("_DSC1764.JPG");
-dark4 = imread("_DSC1765.JPG");
-dark5 = imread("_DSC1766.JPG");
-flat1 = imread("_DSC1767.JPG");
-flat2 = imread("_DSC1768.JPG");
-flat3 = imread("_DSC1769.JPG");
-flat4 = imread("_DSC1770.JPG");
-flat5 = imread("_DSC1771.JPG");
-
-
 coins = cell(1,12)
 [mean_bias,mean_dark,norm_flat] = Imean();
 x = linspace(72,83,12);
